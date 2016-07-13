38ccd7cf30586b94ffc5304366249ced4d42042e
jyami <jyami@hoge>
2016-07-13 18:00:43 +0900

    denodeify()をes6denodeifyとしてモジュール化

--- learnyounode/program04_promise.js
+++ learnyounode/program04_promise.js
@@ -1,23 +1,6 @@
-function denodeify(nodeFunc){
-    var baseArgs = Array.prototype.slice.call(arguments, 1);
-    return function() {
-        var nodeArgs = baseArgs.concat(Array.prototype.slice.call(arguments));
-        return new Promise((resolve, reject) => {
-            nodeArgs.push((error, data) => {
-                if (error) {
-                    reject(error);
-                } else if (arguments.length > 2) {
-                    resolve(Array.prototype.slice.call(arguments, 1));
-                } else {
-                    resolve(data);
-                }
-            });
-            nodeFunc.apply(null, nodeArgs);
-        });
-    }
-}
+var es6denodeify = require('./es6denodeify');
 
-var readFile = denodeify(require('fs').readFile);
+var readFile = es6denodeify(require('fs').readFile);
 
 readFile(process.argv[2], 'utf8').then(function (value) {
     console.log(value.split('\n').length-1);
