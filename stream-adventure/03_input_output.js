6cba29a60090e32b58dcce703028496b9fc2a413
jyami <jyami@hoge>
2016-07-17 11:59:02 +0900

    #3 add 03_input_output.js

--- stream-adventure/03_input_output.js
+++ stream-adventure/03_input_output.js
@@ -1,11 +1,18 @@
 var through2 = require('through2');
+var split = require('split');
 
+var count = 1;
 var trans = through2.obj(function(chunk, enc, callback) {
-    this.push(chunk.toString().toUpperCase());
+    var line = chunk.toString();
+    if (count % 2 == 0) line = line.toUpperCase();
+    else line = line.toLowerCase(); 
+    this.push(line + '\n');
+    count++;
     callback();
 });
 
 
 process.stdin
+.pipe(split())
 .pipe(trans)
 .pipe(process.stdout);
